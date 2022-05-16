83ca8b826e15ddc39600a0c0213b76fe78c1669d
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-16 14:26:08 +0300

    upgraded pos tests

--- src/test/java/ru/netology/test/BookCardTest.java
+++ src/test/java/ru/netology/test/BookCardTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.ownText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
@@ -25,8 +26,8 @@ public class BookCardTest {
         $x("//span[@data-test-id = \"name\"]//input").setValue("Макаров Иван");
         $x("//span[@data-test-id = \"phone\"]//input").setValue("+79999999999");
         $x("//span[@class = \"checkbox__box\"]").click();
-        $x("//button[contains(@class, \"button_view_extra\")]").click();
-        $(withText("Успешно")).should(visible, Duration.ofSeconds(15));
-        $(byText("19.05.2022")).should(visible);
+        $x("//*[text() = \"Забронировать\"]").click();
+        $(withText("Успешно!")).should(visible, Duration.ofSeconds(15));
+        $(".notification__content").should(ownText("19.05.2022"));
     }
 }
