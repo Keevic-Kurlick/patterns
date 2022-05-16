dae12a344ac8f97785c88daf17059bfad74ce12c
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-16 15:50:03 +0300

    added flexibility in date field

--- src/test/java/ru/netology/test/BookCardTest.java
+++ src/test/java/ru/netology/test/BookCardTest.java
@@ -1,33 +1,42 @@
 package ru.netology.test;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.ownText;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class BookCardTest {
+    private final int daysArea = 3;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private final LocalDate date = LocalDate.now();
+    private final LocalDate acceptableDate = date.plusDays(daysArea);
+    private final String finalDate = (acceptableDate.format(formatter));
 
     @Test
-    void shouldBookCard() {
-        Configuration.holdBrowserOpen=true;
-
+    void shouldBookCardWithProperValues() {
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
+
         $x("//input[@placeholder =\"Город\"]").setValue("Москва");
-        $x("//input [@placeholder = \"Дата встречи\"]").setValue("19.05.2022");
+        $x("//input [@placeholder = \"Дата встречи\"]").click();
+        $x("//input [@placeholder = \"Дата встречи\"]").sendKeys(Keys.CONTROL + "A");
+        $x("//input [@placeholder = \"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
+        $x("//input [@placeholder = \"Дата встречи\"]").setValue(finalDate);
         $x("//span[@data-test-id = \"name\"]//input").setValue("Макаров Иван");
         $x("//span[@data-test-id = \"phone\"]//input").setValue("+79999999999");
         $x("//span[@class = \"checkbox__box\"]").click();
         $x("//*[text() = \"Забронировать\"]").click();
+
         $(withText("Успешно!")).should(visible, Duration.ofSeconds(15));
-        $(".notification__content").should(ownText("19.05.2022"));
+        $(".notification__content").should(ownText(finalDate));
     }
 }
