19cc1dca01d89d79985a551aefd86eabf757fafe
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-17 15:55:49 +0300

    deleted useless import

--- src/test/java/ru/netology/test/BookCardTest.java
+++ src/test/java/ru/netology/test/BookCardTest.java
@@ -1,7 +1,6 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
