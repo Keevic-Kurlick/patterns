81844b5265e2664a696144c3ab6d44347b22c714
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-17 15:50:27 +0300

    correct city test

--- src/test/java/ru/netology/test/BookCardTest.java
+++ src/test/java/ru/netology/test/BookCardTest.java
@@ -27,7 +27,7 @@ public class BookCardTest {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
 
-        $x("//input[@placeholder =\"Город\"]").setValue("Бердянск");
+        $x("//input[@placeholder =\"Город\"]").setValue("Москва");
         $x("//input [@placeholder = \"Дата встречи\"]").sendKeys(Keys.CONTROL + "A");
         $x("//input [@placeholder = \"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
         $x("//input [@placeholder = \"Дата встречи\"]").setValue(finalDate);
