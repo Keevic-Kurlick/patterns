dc7ac3bd0603b883f5cd762fe855b486561492f6
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-18 03:02:45 +0300

    added util class

--- src/test/java/ru/netology/entities/RegistrationInfo.java
+++ src/test/java/ru/netology/entities/RegistrationInfo.java
@@ -1,11 +1,23 @@
 package ru.netology.entities;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Data
-public class GeneratorByName {
+public class RegistrationInfo {
     private final String name;
     private final String phoneNumber;
+
+    public RegistrationInfo(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
 }
--- src/test/java/ru/netology/test/BookCardTest.java
+++ src/test/java/ru/netology/test/BookCardTest.java
@@ -3,6 +3,8 @@ package ru.netology.test;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+import ru.netology.entities.RegistrationInfo;
+import ru.netology.utils.DataGenerator;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -15,27 +17,57 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class BookCardTest {
-    private final int daysArea = 5;
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    private final LocalDate date = LocalDate.now();
-    private final LocalDate acceptableDate = date.plusDays(daysArea);
-    private final String finalDate = (acceptableDate.format(formatter));
+
+    public String generateDate(int daysPeriod) {
+        return (LocalDate.now()
+                .plusDays(daysPeriod)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+    }
 
     @Test
-    void shouldBookCardWithProperValues() {
+    void shouldBookCardWithProperValuesDataGen() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
+        RegistrationInfo user = DataGenerator.Registration.generateByName("ru");
 
         $x("//input[@placeholder =\"Город\"]").setValue("Москва");
         $x("//input [@placeholder = \"Дата встречи\"]").sendKeys(Keys.CONTROL + "A");
         $x("//input [@placeholder = \"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
-        $x("//input [@placeholder = \"Дата встречи\"]").setValue(finalDate);
-        $x("//span[@data-test-id = \"name\"]//input").setValue("Макаров Иван");
-        $x("//span[@data-test-id = \"phone\"]//input").setValue("+79999999999");
+        $x("//input [@placeholder = \"Дата встречи\"]").setValue(generateDate(3));
+        $x("//span[@data-test-id = \"name\"]//input").setValue(user.getName());
+        $x("//input[@placeholder = \"+7 000 000 00 00\"]").setValue(user.getPhoneNumber());
         $x("//span[@class = \"checkbox__box\"]").click();
-        $x("//*[text() = \"Забронировать\"]").click();
+        $x("//*[text() = \"Запланировать\"]").click();
+
+        $(withText("Успешно!")).should(visible);
+        $(".notification__content").should(ownText(generateDate(3)));
+    }
+
+    @Test
+    void shouldBookCardWithProperValuesSecondTimeGen() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        RegistrationInfo user = DataGenerator.Registration.generateByName("ru");
+        $x("//input[@placeholder =\"Город\"]").setValue("Москва");
+        $x("//input [@placeholder = \"Дата встречи\"]").sendKeys(Keys.CONTROL + "A");
+        $x("//input [@placeholder = \"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
+        $x("//input [@placeholder = \"Дата встречи\"]").setValue(generateDate(3));
+        $x("//span[@data-test-id = \"name\"]//input").setValue(user.getName());
+        $x("//input[@placeholder = \"+7 000 000 00 00\"]").setValue(user.getPhoneNumber());
+        $x("//span[@class = \"checkbox__box\"]").click();
+        $x("//*[text() = \"Запланировать\"]").click();
 
         $(withText("Успешно!")).should(visible, Duration.ofSeconds(15));
-        $(".notification__content").should(ownText(finalDate));
+        $$(".notification__content").filter(visible).first().should(ownText(generateDate(3)));
+        $x("//*[text() = \"Запланировать\"]").click();
+        $(withText("Необходимо подтверждение")).should(visible, Duration.ofSeconds(3));
+        $(".notification__content button").click();
+        $$(".notification__content").filter(visible).last().should(ownText(generateDate(3)));
+
     }
+
+    public void print(String name, String phone){
+        System.out.println("Имя: " + name + " Номер: " + phone);
+    }
+
 }
